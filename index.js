9ebd6a75cc30d27e9892bc6c151f91acc09a6685
unknown <Jgossett73@gmail.com>
2022-08-15 17:33:21 -0400

    added cards and generate questions

--- index.js
+++ index.js
@@ -1,12 +1,132 @@
 
 const renderOutputHTML = require('./src/renderOutputHTML')
 
-const inquirer = require("inquirer");
-const fs = require("fs");
+
 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const employees = []
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const employeeArray = []
+
+
+const hireManager = () => {
+    return inquirer.prompt (
+ [
+    {
+        type:'input',
+        name:'name',
+        message: 'Lets select a manager for our team, What is the Managers Name?'
+    },
+    {
+        type:'input',
+        name:'id',
+        message:'Please assign an Employee ID'
+    },
+    { 
+        type:'input',
+        name: 'email',
+        message :'Please create/designate Managers Email'
+    },
+    {
+        type:'input',
+        name:'officeNumber',
+        message :'please designate an office number for the Manager'
+    }
+])
+.then(managerData) => {
+    const {name,id,email,officeNumber} = managerData;
+    const manager = new Manager (name,id,email,officeNumber);
+    employeeArray.push(manager);
+    console.log(manager);
+};
+
+const hireEmployee = () => {
+    console.log('Congratulations!!');
+    return inquirer.prompt ([
+        {
+            type: 'checkbox',
+            name: 'position',
+            message:'Which role is being hired?',
+            choices: ['Intern', 'Engineer']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message:'Please enter the new hires name'
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'Please enter an email address for new employee'
+        },
+        { 
+            type:'input',
+            name:'github',
+            message:'Please enter a Github username for new employee',
+            when: (input) => input.position ==="Engineer"
+        },
+        {
+            type:'input',
+            name: 'school',
+            message:'Enter the school name of new Intern'
+        },
+        {
+            type:'confirm',
+            name:'confirmHire',
+            message: ' Do you have other members to add to your team?',
+            default:false
+        }
+    ])
+    .then(employeeData) => {
+        let {name,id,email,position,github,school,confirm} = employeeData;
+        let employee;
+        if (position === "Engineer") {
+            employee = new Engineer (name,id,email,github);
+            console.log(employee);
+        }
+        else if (position === "Intern") {
+            employee = new Intern (name,id,email,school);
+            console.log(employee);
+        }
+        employeeArray.push(employee);
+        if(confirmHire) {
+            return hireEmployee(employeeArray) }
+            else {
+                return employeeArray;
+            }
+        };
+
+        const writeFile = data => {
+            fs.writeFile('\Users\Gossett\OneDrive\Desktop\GTchal10\src\renderOutputHTML.js', data, err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                else {
+                    console.log('your team has been created')
+                }
+            })};
+            hireManager()
+            .then(hireEmployee)
+            .then(employeeArray => {
+                return renderOutputHTML(employeeArray);
+            })
+            .then(pageHTML => {
+                return writeFile(pageHTML);
+            });
+
+        
+
+    
+    
+    
+
+    
+    
+
+
 
 
--- src/renderOutputHTML.js
+++ src/renderOutputHTML.js
@@ -1,4 +1,4 @@
-const renderOutputHTML = function(manager)
+const renderManager = function(manager)
 {
     return `
     <div class ="container">
@@ -11,10 +11,52 @@ const renderOutputHTML = function(manager)
 
     <div class="card-body">
     <p class="id">ID: ${manager.id}</p>
-    <P class="email">Email: <a href="email:${manager.email}">${manager.email}</a></p>
+    <p class="email">Email: <a href="email:${manager.email}">${manager.email}</a></p>
+    <p class ="office">office number:${manager.officeNumber}</p>
     </div>
     </div>
     </div> 
     `;}
+
+    const renderEngineer = function(engineer)
+{
+    return `
+    <div class ="container">
+
+    <div class ="card">
+    <div class ="card-header">
+    <h2>${engineer.name}</h2>
+    <h3>Manager</h3>
+    </div>
+
+    <div class="card-body">
+    <p class="id">ID: ${engineer.id}</p>
+    <p class="email">Email: <a href="email:${engineer.email}">${engineer.email}</a></p>
+    <p class ="github">Github: <a href ="https://github.com/${engineer.github}"<${engineer.github}</a></p>
+    </div>
+    </div>
+    </div> 
+    `;}
+
+    const renderIntern= function(intern)
+{
+    return `
+    <div class ="container">
+
+    <div class ="card">
+    <div class ="card-header">
+    <h2>${intern.name}</h2>
+    <h3>Manager</h3>
+    </div>
+
+    <div class="card-body">
+    <p class="id">ID: ${intern.id}</p>
+    <p class="email">Email: <a href="email:${intern.email}">${intern.email}</a></p>
+    <p class="school">School: ${intern.school}</p>
+    </div>
+    </div>
+    </div> 
+    `;}
+    
 
     module.exports = renderOutputHTML;
